859132d3ee7088a3afce4787df0f4683bc3fc0eb
Paul Ferlito <pferlito@gmail.com>
2013-10-02 01:59:48 -0700

    Bug fixes.

---
+++ modules/contrib/examples/email_example/lib/Drupal/email_example/Form/EmailExampleGetFormPage.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\email_example\Form\EmailExampleGetFormPage.
+ */
+
+namespace Drupal\email_example\Form;
+
+use Drupal\Core\Form\FormInterface;
+
+/**
+ * File test form class.
+ */
+class EmailExampleGetFormPage implements FormInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormID() {
+    return 'email_example';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $form['intro'] = array(
+      '#markup' => t('Use this form to send a message to an e-mail address. No spamming!'),
+    );
+    $form['email'] = array(
+      '#type' => 'textfield',
+      '#title' => t('E-mail address'),
+      '#required' => TRUE,
+    );
+    $form['message'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Message'),
+      '#required' => TRUE,
+    );
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Submit'),
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, array &$form_state) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, array &$form_state) {
+  }
+}
